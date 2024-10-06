ed8c5924a644caecd9a2cd011c761dbbf5f94776
Marko Askovic <marko.askovic@astree-software.fr>
2024-10-06 23:18:10 +0200

    commit

--- auth/src/index.js
+++ auth/src/index.js
@@ -4,14 +4,14 @@ const mongoose = require('mongoose')
 const { port, db, apiUrl } = require('./configuration')
 const { connectDb } = require('./helpers/db')
 
-const app = express()
+const app = express();
 
 
 app.get('/test', (req, res) => {
     res.send('Our authentication server is working correctly');
 })
 
-app.get('/api/currentUser', (req, res) => {
+app.get('/currentUser', (req, res) => {
     return res.json({
         id: "1234",
         email: "foo@gmail.com"
@@ -24,6 +24,28 @@ app.get('/testwithapidata', (req, res) => {
             testapidata: response.data.testapidata
         })
     })
+});
+
+app.post('/register', async (req, res) => {
+    console.log('Logic to registrate a new user');
+    // If the registration is successful, then send an email.
+    try {
+        // Send POST request to the mailing service : here, the mailer service is included into api service
+        const response = await axios.post(apiUrl + '/send-email', {
+            to: 'recipient@example.com',
+            subject: 'Registration of user successful',
+            text: 'User registered successfully!',
+        });
+
+        // Send success response
+        res.status(200).json({
+            message: 'Email sent successfully!',
+            response: response.data,
+        });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json({ error: 'Failed to send email.' });
+    }
 });
 
 const startServer = () => {
